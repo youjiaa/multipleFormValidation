544f3bab0ac5a3c13f9e726faddcfcd0e1dae4ce
uimm258 <uimm258@gmail.com>
2021-10-03 17:30:30 -0400

    update

--- forms/src/standard-forms/contact.js
+++ forms/src/standard-forms/contact.js
@@ -3,57 +3,104 @@ import "./style.css";
 
 const regex = {
     name: /^[a-z ,.'-]+$/i,
-    emailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     phone: /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-    text: /^[a-zA-Z\s]*$/
+    message: /^[a-zA-Z\s]*$/
 };
 
 export default function Contact() {
     const [input, setInput] = React.useState({
         errors: false,
         touched: false,
-        value: ""
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        message:""
     })
 
     const handleChange = (e) => {
         let value = e.target.value;
-        const isValid = regex.text(value);
+        const isValidName = regex.name.test(value);
+        const isValidEmail = regex.email.test(value);
+        const isValidPhone = regex.phone.test(value);
+        const isValidText = regex.message.test(value);
+        console.log(isValidName)
 
         setInput({
-            error: !isValid,
+            error: !isValidName || !isValidEmail || !isValidPhone || isValidText,
             touched: true,
-            value: value
+            firstName: value,
+            lastName: value,
+            phone: "",
+            email: "",
+            message: ""
         });
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("hello")
+    const handleSubmit = () => {
+        console.log("no submit function yet");
     };
 
     const handleBlur = () => {
-        console.log("hi")
+        console.log("no blur function yet");
     }
 
+    console.log("errors", input.errors)
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="firstName" style={{ display: "block" }}>
                     First Name
                 </label>
-                <inputs
+                <input
                     id="firstName"
                     placeholder="Enter your first name"
                     type="text"
-                    value={input}
+                    value={input.firstName}
                     onChange={handleChange}
                     onBlur={handleBlur}
                     className={
-                        input.errors.name && input.touched.name
+                        input.errors && input.touched
                             ? "text-input error"
                             : "text-input"
                     }
                 />
+
+                <label htmlFor="lastName" style={{ display: "block" }}>
+                    Last Name
+                </label>
+                <input
+                    id="lastName"
+                    placeholder="Enter your last name"
+                    type="text"
+                    value={input.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                        input.errors && input.touched
+                            ? "text-input error"
+                            : "text-input"
+                    }
+                />
+
+                <label htmlFor="phone" style={{ display: "block" }}>
+                    Phone Number
+                </label>
+                <input
+                    id="phone"
+                    placeholder="Enter your phone"
+                    type="text"
+                    value={input.phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                        input.errors && input.touched
+                            ? "text-input error"
+                            : "text-input"
+                    }
+                />
+
             </form>
         </>
     )
