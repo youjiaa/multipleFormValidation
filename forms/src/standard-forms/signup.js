a64cf96d1c4c7199e26ee8c5693052ab1be0bde1
youjiaa <42870529+youjiaa@users.noreply.github.com>
2021-10-04 16:23:39 -0400

    compelete standard-signup

--- forms/src/standard-forms/signup.js
+++ forms/src/standard-forms/signup.js
@@ -4,11 +4,15 @@ export default function SignUp() {
     const [fName, setfName] = useState('');
     const [lName, setlName] = useState('');
     const [email, setEmail] = useState('');
-
+    const [pwd, setPwd] = useState('');
+  const [repwd, setRepwd] = useState('');
+  
     const [fNameError, setfNameerror] = useState(false);
     const [lNameError, setlNameerror] = useState(false);
-    const [emailError, setEmailError] = useState("");
-
+  const [emailError, setEmailError] = useState("");
+      const [pwdError, setPwderror] = useState("");
+  const [repwdError, setRepwderror] = useState(false);
+  
   const fNameBlurData = () => {
     if (fName === '') {
     setfNameerror(true);
@@ -40,14 +44,48 @@ export default function SignUp() {
       }
   };
     
+      const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    const validatePwd = () => {
+        if (pwd === '') {
+            setPwderror("Password is required");
+            return;
+        } else {
+            if (pwdRegex.test(pwd)) {
+                setPwderror('')
+                return;
+            } else {
+            setPwderror( "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character");
+            }
+      }
+    };
+
+  const repwdBlurData = () => {
+    if (repwd !== pwd) {
+    setRepwderror(true);
+      return;
+    }
+    setRepwderror(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(JSON.stringify({
+      "first name": event.target.fName.value, 
+      "last name": event.target.lName.value,
+      "email": event.target.email.value,
+      "password": event.target.pwd.value,
+      "re-password": event.target.repwd.value,
+    }))
+  }
 
   return (
     <div classfName="App">
-      <form>
+      <form onSubmit={handleSubmit}>
       <label>First Name:</label>
         <input
           onBlur={() => fNameBlurData()}
           type="text"
+          name="fName"
           value={fName}
           onChange={e => setfName(e.target.value)}
         />
@@ -58,6 +96,7 @@ export default function SignUp() {
         <input
           onBlur={() => lNameBlurData()}
           type="text"
+          name="lName"
           value={lName}
           onChange={e => setlName(e.target.value)}
         />
@@ -66,14 +105,40 @@ export default function SignUp() {
 
     <label>Email:</label>          
     <input
-        type="email"
-        type="text"
+          type="text"
+          name="email"
         value={email}
         onBlur={validateEmail}
         onChange={e => setEmail(e.target.value)}
       />
-      {emailError !== '' ? <div className="input-feedback">{emailError}</div> : null}
+        {emailError !== '' ? <div className="input-feedback">{emailError}</div> : null}
+         <br />
 
+   <label>Password:</label>          
+    <input
+          type="password"
+          name="pwd"
+        value={pwd}
+        onBlur={validatePwd}
+        onChange={e => setPwd(e.target.value)}
+      />
+        {pwdError !== '' ? <div className="input-feedback">{pwdError}</div> : null}
+        <br />
+        
+      <label>Re-enter Password:</label>          
+    <input
+          type="password"
+          name="repwd"
+        value={repwd}
+        onBlur={repwdBlurData}
+        onChange={e => setRepwd(e.target.value)}
+        />
+        {repwdError ? <div className="input-feedback">Passwords must match</div> : null}
+        <br />
+        
+        <button type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
