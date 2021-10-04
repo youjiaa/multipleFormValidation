ac3375790a9c8f4861b70b1a1294aeeae180d525
youjiaa <42870529+youjiaa@users.noreply.github.com>
2021-10-04 13:57:02 -0400

    update signup

--- forms/src/standard-forms/accout.js
+++ forms/src/standard-forms/accout.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Accout() {
     return (
         <div>
-            
+            !!!
         </div>
     )
 }
--- forms/src/standard-forms/signup.js
+++ forms/src/standard-forms/signup.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React,{ useState }  from 'react'
 
 export default function SignUp() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const [fName, setfName] = useState('');
+    const [lName, setlName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [fNameError, setfNameerror] = useState(false);
+    const [lNameError, setlNameerror] = useState(false);
+    const [emailError, setEmailError] = useState("");
+
+  const fNameBlurData = () => {
+    if (fName === '') {
+    setfNameerror(true);
+      return;
+    }
+    setfNameerror(false);
+  };
+    
+  const lNameBlurData = () => {
+    if (lName === '') {
+    setlNameerror(true);
+      return;
+    }
+    setlNameerror(false);
+  };
+    
+    const emailRegex = /\S+@\S+\.\S+/;
+    const validateEmail = () => {
+        if (email === '') {
+            setEmailError("Email is required");
+            return;
+        } else {
+            if (emailRegex.test(email)) {
+                setEmailError('')
+                return;
+            } else {
+            setEmailError("Invalid email address");
+            }
+      }
+  };
+    
+
+  return (
+    <div classfName="App">
+      <form>
+      <label>First Name:</label>
+        <input
+          onBlur={() => fNameBlurData()}
+          type="text"
+          value={fName}
+          onChange={e => setfName(e.target.value)}
+        />
+    {fNameError ? <div className="input-feedback">First Name is required</div> : null}           
+ <br />
+
+    <label>Last Name:</label>
+        <input
+          onBlur={() => lNameBlurData()}
+          type="text"
+          value={lName}
+          onChange={e => setlName(e.target.value)}
+        />
+    {lNameError ? <div className="input-feedback">Last Name is required</div> : null}          
+              <br />
+
+    <label>Email:</label>          
+    <input
+        type="email"
+        type="text"
+        value={email}
+        onBlur={validateEmail}
+        onChange={e => setEmail(e.target.value)}
+      />
+      {emailError !== '' ? <div className="input-feedback">{emailError}</div> : null}
+
+      </form>
+    </div>
+  );
 }
