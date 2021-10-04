cccf81d5c5990995cb3482088044405594b96341
Mochi778 <qingyuguo0204@gmail.com>
2021-10-04 19:37:18 -0400

    update advanced sign-in form

--- forms/src/advanced-forms/signin.js
+++ forms/src/advanced-forms/signin.js
@@ -1,39 +1,73 @@
 import React from 'react'
 import './style.css'
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function Signin() {
-  const formik = useFormik({
-    initialValues: { email: "" }, 
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    }
-  });
+  // const formik = useFormik({
+  //   initialValues: { email: "" }, 
+  //   onSubmit: values => {
+  //     alert(JSON.stringify(values, null, 2));
+  //   }
+  // });
   return (
- <> 
+  <> 
      <h1>This is the Sign In Form</h1>
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-        />
-        
-      <label>Password:</label>
-      <input
-        type="password"
-        name="password"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.password}
-      />
-      <br />
+     <Formik 
+      initialValues={{email: '', password: ''}}
+      validate={values => {
+        const errors = {};
+        if(!values.email){
+          errors.email = 'Email is Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        if(!values.password){
+          errors.password = 'Password is Required';
+        } else if (
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/        
+        ) {
+          errors.password = 'Invalid password input';
+        }
+          return errors;
+      }}
 
-      <button type="submit">Submit</button>
-      </form>
-      </>
+          onSubmit={({ setSubmitting }) => {
+            alert('Form validated successfully')
+            setSubmitting(false);
+        }}
+      >
+      
+      {({values, errors, touched, isSubmitting}) => (
+        <Form>
+          <label>Email:</label>
+          <Field 
+            type='email' 
+            name='email' 
+            placeholder='Enter email'
+            className={`form-control ${
+              touched.email && errors.email ? "is-invalid" : ""}`}
+          />
+          <ErrorMessage className='input-feedback' name='email' component='div' />
+          <br />
+
+          <label>Password:</label>
+          <Field
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            className={`form-control ${
+              touched.password && errors.password ? "is-invalid" : ""}`}
+          />
+          <ErrorMessage className='input-feedback' name='password' component='div' />
+          <br />
+          <button type="submit" disabled={isSubmitting}>
+              Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </>
   );
 }
