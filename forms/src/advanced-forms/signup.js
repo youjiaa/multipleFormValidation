0a53db31a67099cc4db29039d516cbd328544a5d
youjiaa <42870529+youjiaa@users.noreply.github.com>
2021-10-05 11:57:49 -0400

    add error message for standard signin, add some comments

--- forms/src/advanced-forms/signup.js
+++ forms/src/advanced-forms/signup.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import './style.css'
-import * as Yup from 'yup';
+import "./style.css";
+import * as Yup from "yup";
 
 export default function Signup() {
   return (
     <>
       <h1>This is the Sign Up Form</h1>
       <Formik
-        initialValues={{ firstName: "", lastName: "" , email: '', password: '' }}
-        validate={values => {
+        initialValues={{ firstName: "Patrick", lastName: "", email: "", password: "", rePassword:"" }}
+        validate={(values) => {
           const errors = {};
           if (!values.firstName) {
             errors.firstName = "first name is Required";
@@ -17,25 +17,27 @@ export default function Signup() {
           if (!values.lastName) {
             errors.lastName = "last name is Required";
           }
-           if (!values.email) {
-           errors.email = 'email is Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
+          if (!values.email) {
+            errors.email = "email is Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
           return errors;
         }}
-
         validationSchema={Yup.object({
-          password: Yup.string().required('Password is required')
-            .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-    ),
-          rePassword: Yup.string()
-            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+          password: Yup.string()
+            .required("Password is required")
+            .matches(
+              /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+              "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+            ),
+          rePassword: Yup.string().oneOf(
+            [Yup.ref("password"), null],
+            "Passwords must match"
+          ),
         })}
-
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -43,60 +45,78 @@ export default function Signup() {
           }, 400);
         }}
       >
-        {({          values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit, isSubmitting,isValid,dirty }) => (
+{/*        render prop*/} 
+       {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          isValid,
+          dirty,
+        }) => (
           <Form>
             <label>First Name:</label>
-            <Field type="text" name="firstName" />
-            <ErrorMessage className="input-feedback" name="firstName" component="div" />
+            <Field type="text" name="firstName"
+              onChange={(event) => alert(event.target.value)}
+            />
+            <ErrorMessage
+              className="input-feedback"
+              name="firstName"
+              component="div"
+            />
             <br />
 
             <label>Last Name:</label>
             <Field type="text" name="lastName" />
-            <ErrorMessage className="input-feedback" name="lastName" component="div" />
+            <ErrorMessage
+              className="input-feedback"
+              name="lastName"
+              component="div"
+            />
             <br />
 
             <label>Email:</label>
-             <input
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
             />
-            <div className="input-feedback">{errors.email && touched.email && errors.email}</div>
-            
+            <div className="input-feedback">
+              {errors.email && touched.email && errors.email}
+            </div>
+
             <br />
 
             <label>Password:</label>
             <input
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
             />
             <div className="input-feedback">
-            {errors.password && touched.password && errors.password}
+              {errors.password && touched.password && errors.password}
             </div>
             <br />
 
             <label>Re-enter Password:</label>
             <Field type="password" name="rePassword" />
             {errors.rePassword && touched.rePassword && (
-                <div className="input-feedback">{errors.rePassword}</div>
-              )}   
+              <div className="input-feedback">{errors.rePassword}</div>
+            )}
             <br />
-            <button type="submit" disabled={!(isValid)}>
+            <button type="submit" disabled={!isValid}>
               Submit
             </button>
           </Form>
         )}
       </Formik>
     </>
-  )
+  );
 }
--- forms/src/standard-forms/signup.js
+++ forms/src/standard-forms/signup.js
@@ -1,22 +1,21 @@
-import React,{ useState }  from 'react'
+import React, { useState } from "react";
 
 export default function SignUp() {
-    const [fName, setfName] = useState('');
-    const [lName, setlName] = useState('');
-    const [email, setEmail] = useState('');
-    const [pwd, setPwd] = useState('');
-  const [repwd, setRepwd] = useState('');
-  
-    const [fNameError, setfNameerror] = useState(false);
-    const [lNameError, setlNameerror] = useState(false);
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [repwd, setRepwd] = useState("");
+
+  const [fNameError, setfNameerror] = useState(false);
+  const [lNameError, setlNameerror] = useState(false);
   const [emailError, setEmailError] = useState("");
-      const [pwdError, setPwderror] = useState("");
+  const [pwdError, setPwderror] = useState("");
   const [repwdError, setRepwderror] = useState(false);
-  
-      const [dis, setDis] = useState(true);
 
+  const [dis, setDis] = useState(true);
 
-  const fNameBlurData = () => {
+  const validateFname = () => {
     if (!fName) {
       // console.log(Boolean(fName))
       setfNameerror(true);
@@ -26,51 +25,53 @@ export default function SignUp() {
     return true;
   };
 
-  
-  const lNameBlurData = () => {
+  const validateLname = () => {
     if (!lName) {
-        setlNameerror(true);
-        return false;
+      setlNameerror(true);
+      return false;
     }
     setlNameerror(false);
     return true;
   };
-    
+
   const emailRegex = /\S+@\S+\.\S+/;
   const validateEmail = () => {
     if (!email) {
-          setEmailError("Email is required");
-          return false;
-      } else {
+      setEmailError("Email is required");
+      return false;
+    } else {
       if (emailRegex.test(email)) {
-              setEmailError('')
-              return true;
-          } else {
+        setEmailError("");
+        return true;
+      } else {
         setEmailError("Invalid email address");
         return false;
-          }
       }
-  };
-    
-  const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-const validatePwd = () => {
-  if (!pwd) {
-        setPwderror("Password is required");
-        return false;
-    } else {
-    if (pwdRegex.test(pwd)) {
-          setPwderror('')
-          return true;
-        } else {
-        setPwderror( "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character");
-    return false;    
     }
+  };
+
+  const pwdRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const validatePwd = () => {
+    if (!pwd) {
+      setPwderror("Password is required");
+      return false;
+    } else {
+      if (pwdRegex.test(pwd)) {
+        setPwderror("");
+        return true;
+      } else {
+        setPwderror(
+          "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        );
+        return false;
       }
-    };
+    }
+  };
 
   const repwdBlurData = () => {
     if (repwd !== pwd) {
-    setRepwderror(true);
+      setRepwderror(true);
       return false;
     }
     setRepwderror(false);
@@ -78,81 +79,106 @@ const validatePwd = () => {
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    if (fNameBlurData() && lNameBlurData() && validateEmail() && validatePwd() && repwdBlurData()) {
-      
-    alert(JSON.stringify({
-      "first name": event.target.fName.value, 
-      "last name": event.target.lName.value,
-      "email": event.target.email.value,
-      "password": event.target.pwd.value,
-      "re-password": event.target.repwd.value,
-    }))
+    event.preventDefault();
+    
+    let isFnamevalid = validateFname()
+    let isLnamevalid = validateLname()
+    let isEmailvalid = validateEmail()
+    let isPwdvalid = validatePwd()
+    let isRepwdvalid = repwdBlurData()
+    // if (//evoke and get return value instead of 
+    //   validateFname() &&
+    //   validateLname() &&
+    //   validateEmail() &&
+    //   validatePwd() &&
+    //   repwdBlurData()
+    // ) {
+    if (isFnamevalid && isLnamevalid  && isEmailvalid &&  isPwdvalid && isRepwdvalid){
+      alert(
+        JSON.stringify({
+          "first name": fName,
+          "last name": event.target.lName.value,
+          email: event.target.email.value,
+          password: event.target.pwd.value,
+          "re-password": event.target.repwd.value,
+        })
+      );
     }
-  }
+  };
 
   return (
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>First Name:</label>
-        <input
-          onBlur={() => fNameBlurData()}
-          type="text"
-          name="fName"
-          value={fName}
-          onChange={e => setfName(e.target.value)}
-        />
-    {fNameError ? <div className="input-feedback">First Name is required</div> : null}           
- <br />
+      <input
+        onBlur={validateFname} // not passing data value so no anon function
+        type="text"
+        name="fName"
+        value={fName} //one way data binding from model to view
+        onChange={(e) => setfName(e.target.value)}//the second way of event binding from view to model
+      />
+      {fNameError ? (
+        <div className="input-feedback">First Name is required</div>
+      ) : null}
+      <br />
 
-    <label>Last Name:</label>
-        <input
-          onBlur={() => lNameBlurData()}
-          type="text"
-          name="lName"
-          value={lName}
-          onChange={e => setlName(e.target.value)}
-        />
-    {lNameError ? <div className="input-feedback">Last Name is required</div> : null}          
-              <br />
+      <label>Last Name:</label>
+      <input
+        onBlur={validateLname}
+        type="text"
+        name="lName"
+        value={lName}
+        onChange={(e) => setlName(e.target.value)}
+      />
+      {lNameError ? (
+        <div className="input-feedback">Last Name is required</div>
+      ) : null}
+      <br />
 
-    <label>Email:</label>          
-    <input
-          type="text"
-          name="email"
+      <label>Email:</label>
+      <input
+        type="text"
+        name="email"
         value={email}
         onBlur={validateEmail}
-        onChange={e => setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
       />
-        {emailError !== '' ? <div className="input-feedback">{emailError}</div> : null}
-         <br />
+      {emailError !== "" ? (
+        <div className="input-feedback">{emailError}</div>
+      ) : null}
+      <br />
 
-   <label>Password:</label>          
-    <input
-          type="password"
-          name="pwd"
+      <label>Password:</label>
+      <input
+        type="password"
+        name="pwd"
         value={pwd}
         onBlur={validatePwd}
-        onChange={e => setPwd(e.target.value)}
+        onChange={(e) => setPwd(e.target.value)}
       />
-        {pwdError !== '' ? <div className="input-feedback">{pwdError}</div> : null}
-        <br />
-        
-      <label>Re-enter Password:</label>          
-    <input
-          type="password"
-          name="repwd"
+      {pwdError !== "" ? (
+        <div className="input-feedback">{pwdError}</div>
+      ) : null}
+      <br />
+
+      <label>Re-enter Password:</label>
+      <input
+        type="password"
+        name="repwd"
         value={repwd}
         onBlur={repwdBlurData}
-        onChange={e => setRepwd(e.target.value)}
-        />
-        {repwdError ? <div className="input-feedback">Passwords must match</div> : null}
-        <br />
-        
-      <button type="submit"
-      disabled={fNameError || lName || emailError || pwdError ||repwdError}
+        onChange={(e) => setRepwd(e.target.value)}
+      />
+      {repwdError ? (
+        <div className="input-feedback">Passwords must match</div>
+      ) : null}
+      <br />
+
+      <button
+        type="submit"
+        disabled={fNameError || lNameError || emailError || pwdError || repwdError}
       >
-          Submit
-        </button>
-      </form>
+        Submit
+      </button>
+    </form>
   );
 }
