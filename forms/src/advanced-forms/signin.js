a7b196993e021721e6a8b27637c7877a4f6c1e34
Mochi778 <qingyuguo0204@gmail.com>
2021-10-04 20:26:02 -0400

    update

--- forms/src/advanced-forms/signin.js
+++ forms/src/advanced-forms/signin.js
@@ -21,7 +21,7 @@ export default function Signin() {
         } else if (
           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
         ) {
-          errors.email = 'Invalid email address';
+          errors.email = 'Please enter valid email address.';
         }
         if(!values.password){
           errors.password = 'Please enter your password'; }
